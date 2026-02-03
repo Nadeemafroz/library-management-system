acac35b207087680688a70fb657c28576475b65a
Nadeem Afroz <nadeemafroz12@gmail.com>
2026-02-03 07:08:46 +0530

    update url

--- frontend/src/layouts/HomePage/components/Carousel.tsx
+++ frontend/src/layouts/HomePage/components/Carousel.tsx
@@ -12,7 +12,7 @@ export const Carousel = () => {
 
     useEffect(() => {
         const fetchBooks = async () => {
-            const baseUrl: string = "https://api.nadeem.sbs/api/books";
+            const baseUrl: string = "https://api.nadeem.sbs/books";
 
             const url: string = `${baseUrl}?page=0&size=9`;
 
